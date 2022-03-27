1eea025cf4e2484a7dbdc381d546834b02619b3b
Sascvotch <sascvotch@yandex.ru>
2022-03-27 10:29:39 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skypro_employees/SkyproEmployeesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeesApplication.class, args);
+	}
+
+}
